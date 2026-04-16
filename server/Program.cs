6955a5b703466768826ba8e2d795b34c2c1ba69e
kazikSUPER <vitazvir@gmail.com>
2026-04-16 12:56:34 +0300

    feat: integrate swagger for api documentation

---
+++ server/Program.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
